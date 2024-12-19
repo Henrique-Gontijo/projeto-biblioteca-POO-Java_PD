86b07d0a29b2616c46a2bfd5cc8efffb4fa83710
Henrique-Gontijo <henrique.gontijo@pdbomdespacho.com.br>
2024-12-19 09:42:08 -0300

    Um monte de alterações inúteis que serão descartadas logo após este commit

---
+++ mvn-projeto/my-app/src/main/java/com/mycompany/app/Model/Pessoa.java
@@ -0,0 +1,26 @@
+package com.mycompany.app.Model;
+
+public class Pessoa {
+	private String nome;
+	private Livro[] livros;
+
+	public Pessoa(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Livro[] getLivros() {
+		return this.livros;
+	}
+
+	public void setLivros(Livro[] livros) {
+		this.livros = livros;
+	}
+}
